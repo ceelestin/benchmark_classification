b7ad61ad5c8beab456ea94b0d39362bdc920d02c
ceelestin <celest1.eve@gmail.com>
2024-08-01 17:17:43 +0200

    separated sklearn and benchopt API for the Bayes solver

--- benchmark_utils/bayes_estimator.py
+++ benchmark_utils/bayes_estimator.py
@@ -1,63 +1,23 @@
-from benchopt import BaseSolver, safe_import_context
+from benchopt import safe_import_context
 
 # Protect the import with `safe_import_context()`. This allows:
 # - skipping import to speed up autocompletion in CLI.
 # - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     import numpy as np
-    from sklearn.metrics import r2_score
+    from sklearn.base import BaseEstimator, RegressorMixin
 
 
-# The benchmark solvers must be named `Solver` and
-# inherit from `BaseSolver` for `benchopt` to work properly.
-class BayesEstimator(BaseSolver):
-    def __init__(self, noise):
+class BayesEstimator(BaseEstimator, RegressorMixin):
+    def __init__(self, n_features, noise, random_state=None):
         # The following seed must be the same as in the dataset simbayes.py
-        self.rng = np.random.RandomState(42)
-        self.beta = None  # Placeholder for beta
-        self.model = lambda X: self._model_helper(X, noise)
-
-    def _model_helper(self, X, noise):
-        if self.beta is None:
-            self.beta = self.rng.randn(X.shape[1])
-        return X @ self.beta + noise * self.rng.randn(X.shape[0])
+        rng_beta = np.random.RandomState(42)
+        self.beta = rng_beta.randn(n_features)
+        self.noise = noise
+        self.rng = np.random.RandomState(random_state)
 
     def fit(self, X, y):
         pass
 
-    def score(self, X, y):
-        y_pred = self.model(X)
-        return r2_score(y, y_pred)
-
     def predict(self, X):
-        return self.model(X)
-
-    def set_objective(
-            self, X_train, y_train, X_val, y_val,
-            categorical_indicator
-    ):
-        # Define the information received by each solver from the objective.
-        # The arguments of this function are the results of the
-        # `Objective.get_objective`. This defines the benchmark's API for
-        # passing the objective to the solver.
-        # It is customizable for each benchmark.
-        self.X_train, self.y_train = X_train, y_train
-        self.X_val, self.y_val = X_val, y_val
-        self.cat_ind = categorical_indicator
-
-        self.model = self.get_model()  # Includes preprocessor
-
-    def run(self, n_iter):
-        # This is the function that is called to fit the model.
-        # The param n_iter is defined if you change the sample strategy to
-        # other value than "run_once"
-        # https://benchopt.github.io/performance_curves.html
-        self.model.fit(self.X_train, self.y_train)
-
-    def get_result(self):
-        # Returns the model after fitting.
-        # The outputs of this function is a dictionary which defines the
-        # keyword arguments for `Objective.evaluate_result`
-        # This defines the benchmark's API for solvers' results.
-        # it is customizable for each benchmark.
-        return dict(model=self.model)
+        return X @ self.beta + self.noise * self.rng.randn(X.shape[0])
--- solvers/Bayes.py
+++ solvers/Bayes.py
@@ -1,4 +1,4 @@
-from benchopt import safe_import_context
+from benchopt import BaseSolver, safe_import_context
 
 from benchmark_utils.bayes_estimator import BayesEstimator
 
@@ -13,7 +13,7 @@ with safe_import_context() as import_ctx:
 # - getting requirements info when all dependencies are not installed.
 # The benchmark solvers must be named `Solver` and
 # inherit from `BaseSolver` for `benchopt` to work properly.
-class Solver(BayesEstimator):
+class Solver(BaseSolver):
 
     # Name to select the solver in the CLI and to display the results.
     name = 'Bayes'
@@ -27,7 +27,19 @@ class Solver(BayesEstimator):
     # Force solver to run only once if you don't want to record training steps
     sampling_strategy = "run_once"
 
-    def get_model(self):
+    def set_objective(
+            self, X_train, y_train, X_val, y_val,
+            categorical_indicator
+    ):
+        # Define the information received by each solver from the objective.
+        # The arguments of this function are the results of the
+        # `Objective.get_objective`. This defines the benchmark's API for
+        # passing the objective to the solver.
+        # It is customizable for each benchmark.
+        self.X_train, self.y_train = X_train, y_train
+        self.X_val, self.y_val = X_val, y_val
+        self.cat_ind = categorical_indicator
+
         size = self.X_train.shape[1]
         preprocessor = ColumnTransformer(
             [
@@ -39,5 +51,22 @@ class Solver(BayesEstimator):
             ]
         )
 
-        return Pipeline(steps=[("preprocessor", preprocessor),
-                               ("model", BayesEstimator(self.noise))])
+        self.model = Pipeline(steps=[
+            ("preprocessor", preprocessor),
+            ("model", BayesEstimator(n_features=size, noise=self.noise))
+        ])
+
+    def run(self, n_iter):
+        # This is the function that is called to fit the model.
+        # The param n_iter is defined if you change the sample strategy to
+        # other value than "run_once"
+        # https://benchopt.github.io/performance_curves.html
+        self.model.fit(self.X_train, self.y_train)
+
+    def get_result(self):
+        # Returns the model after fitting.
+        # The outputs of this function is a dictionary which defines the
+        # keyword arguments for `Objective.evaluate_result`
+        # This defines the benchmark's API for solvers' results.
+        # it is customizable for each benchmark.
+        return dict(model=self.model)
